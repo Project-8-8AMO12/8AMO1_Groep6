f86edba94cbbe4c37115a83020d32d36b2958c3d
matthijshouwaartstudent <1036981@mborijnland.nl>
2020-06-18 11:59:02 +0200

    Updated and linked navbar to current views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,33 +12,49 @@
 
   <!-- Bootstrap core CSS -->
   <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+<style>
 
+</style>
 </head>
 
 <body>
 
   <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+  <nav class="navbar navbar-expand-lg navbar-light static-top" style="background-color:#ffffff">
     <div class="container">
-      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <a class="navbar-brand" href="/"><img src="{{asset('img/logo.png')}}" style="max-width:100px"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home
-              <span class="sr-only">(current)</span>
+            <a class="nav-link" href="/">Home
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
+            <a class="nav-link" href="/activiteiten">Activiteiten</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Services</a>
+            <a class="nav-link" href="/agenda">Agenda</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
+            <a class="nav-link" href="/cursussen">Cursussen</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/nieuws">Nieuws</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/vereniging">Vereniging</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/stertselaar">Stertselaar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/zwerm">Zwerm</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/galerij">Galerij</a>
           </li>
         </ul>
       </div>
