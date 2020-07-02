b71f85eeb7c72f6fa47bd9cb038588e781807489
sepehrabdoli <sepehrskater@yahoo.com>
2020-07-02 13:09:52 +0200

    welcome page verwijderd en homepage van gemaakt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,37 +1,37 @@
-@extends('layouts.default')
+@extends('layouts.app')
+
 @section('content')
+    <main role="main">
+        <div class="jumbotron">
+            <div class="container" id="homeh1container">
+                <h1 style="color: white;" class="display-3" id="h1home">De imker vereniging
+                    van Leiden</h1>
+                <p style="color: white;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In iaculis nunc sed augue. Porttitor massa id neque al</p>
+        </div>
 
-  <!-- Page Content -->
-  <header>
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-12 text-left">
-        <p class="lead mt-3">Home | Imkers Leiden</p> <!--MAYBE ADD BREADCRUMB-->
-      </div>
-    </div>
-  </div>
-</header>
 
-<div class="row mt-2">
-  <div class="col-sm-1"></div>
-  <div class="col-sm-7">
-    <p>
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    </p>
-  </div>
-  <div class="col-sm-3">IMAGE</div>
-</div>
-
-<hr>
-
-<div class="row mt-2">
-  <div class="col-sm-1"></div>
-  <div class="col-sm-3">IMAGE</div>
-  <div class="col-sm-7">
-    <p>
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    </p>
-  </div>
-</div>
-
+        <div class="container">
+            <!-- Example row of columns -->
+            <div class="row">
+                <div class="col-md-4" id="homecontainers">
+                    <h2>Officieel NBV vereniging</h2>
+                    <p>Er is in ons land een aantal imkersverenigingen, waarvan de Nederlandse Bijenhouders Vereniging (NBV) met bijna 6000 leden, de grootste is. De Imkersvereniging Leiden en omstreken is aangesloten bij de NBV.
+                        Regelmatig organiseert onze vereniging imkersavonden. Op deze bijeenkomsten worden lezingen gehouden en films vertoond. Ook zijn deze avonden het moment waarop imkers elkaar ontmoeten en ervaringen uitwisselen. Soms worden ook excursies georganiseerd. De imkersavonden vinden in principe altijd plaats op de laatste vrijdag van de maand. Ze worden gehouden in de kantine van de Tuinvereniging Cronesteijn aan het Boerenpad 1 te Leiden. </p>
+                    <p><a class="btn btn-warning" href="#" role="button">View details &raquo;</a></p>
+                </div>
+                <div class="col-md-4" id="homecontainers">
+                    <h2>Onze vele bij-zaken</h2>
+                    <p>Als vereniging zijn we bezig bijtjes en organiseren veel leuke activiteiten, promoten wij onze hobby en delen we graag al onze kennis met mede-imker enthousiastelingen.</p>
+                    <p><a class="btn btn-warning" href="#" role="button">View details &raquo;</a></p>
+                </div>
+                <div class="col-md-4" id="homecontainers">
+                    <h2>Onze bijenstallen</h2>
+                    <p></p>
+                    <p><a class="btn btn-warning" href="#" role="button">View details &raquo;</a></p>
+                </div>
+            </div>
+            <hr>
+        </div> <!-- /container -->
+    </main>
 @endsection
+
