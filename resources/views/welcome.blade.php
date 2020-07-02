a76579de21ed7ef3f51956a82261e4be91cb5603
sepehrabdoli <sepehrskater@yahoo.com>
2020-07-02 14:16:58 +0200

    carousel toegevoegd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,37 @@
-@extends('layouts.app')
+@extends('layouts.default')
 
 @section('content')
     <main role="main">
-        <div class="jumbotron">
-            <div class="container" id="homeh1container">
-                <h1 style="color: white;" class="display-3" id="h1home">De imker vereniging
-                    van Leiden</h1>
-                <p style="color: white;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In iaculis nunc sed augue. Porttitor massa id neque al</p>
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="https://placeimg.com/1080/500/animals" alt="First slide">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>My Caption Title (1st Image)</h5>
+                        <p>The whole caption will only show up if the screen is at least medium size.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="https://placeimg.com/1080/500/arch" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="https://placeimg.com/1080/500/nature" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
 
 
